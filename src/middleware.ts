6a5876ded8be0592f62b561d306ad3315c3fd916
manish mahto <manish.mahto@techno-soft.in>
2023-08-18 15:18:44 +0530

    untracked .env

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,9 @@ import { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 
-  
   const token = request.cookies.get("token")?.value;
-
+  console.log('middleware console ????? request.cookies', request.cookies);
+  
   if (!token) {
     return NextResponse.redirect(new URL("/signup", request.url));
   }
